0863556a75cef9b88806d7dd8e71d2c5c67d3271
Julian Montenegro <jdamn.visual@gmail.com>
2026-09-18 06:23:42 -0500

    se crea proyecto

---
+++ beta_1_InnovaCesde/src/main/java/com/example/beta_1_InnovaCesde/Beta1InnovaCesdeApplication.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_InnovaCesde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beta1InnovaCesdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beta1InnovaCesdeApplication.class, args);
+	}
+
+}
---
+++ beta_1_InnovaCesde/src/test/java/com/example/beta_1_InnovaCesde/Beta1InnovaCesdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_InnovaCesde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beta1InnovaCesdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
